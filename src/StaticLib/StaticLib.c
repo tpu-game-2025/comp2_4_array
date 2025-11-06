3849d5426fe4ad35b99949a185494c87be3b870d
g2527002-ctrl <@example.com>
2025-11-06 23:40:00 +0900

    code commit

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -32,30 +32,58 @@ void release(my_array* ar)
 }
 
 // my_array の要素数を変更する(以前の値は要素が同じ範囲で残す)
-void resize(my_array* ar, int n)
-{
-	// ToDo:配列の要素数を変更しよう！(reallocは禁止)
+void resize(my_array* ar, int n) {
+	// ... (n <= 0 の処理)
+
+	// 1. 新しいメモリを確保
+	int* new_addr = (int*)malloc(sizeof(int) * n);
+	if (new_addr == NULL) return; // 確保失敗時はここで終了
+
+	// 2. コピーする要素数を決定
+	int copy_count = (ar->num < n) ? ar->num : n;
+
+	// ★★★ 3. データを新しいメモリへコピー (解放前に行う！) ★★★
+	for (int i = 0; i < copy_count; i++) {
+		new_addr[i] = ar->addr[i];
+	}
+
+	// ★★★ 4. 古いメモリを解放 ★★★
+	if (ar->addr != NULL) {
+		free(ar->addr);
+	}
+
+	// 5. 構造体のメンバーを新しい値で更新
+	ar->addr = new_addr;
+	ar->num = n;
 }
 
-// my_array のindex番目の要素にvalを設定する
-// index が確保されていない場所を指していたら返り値をfalseにする（設定出来たらtrue）
+	
+
 bool set(my_array* ar, int index, int val)
 {
-	// ToDo:配列の要素を変更しよう！
+if (index >= 0 && index < ar->num) {
+		
+		ar->addr[index] = val;
+		return true;
+	}
+	
 	return false;
 }
 
-// my_array のindex番目の要素を取得する
-// index が確保されていない場所を指していたら0を返す
+
 int get(const my_array* ar, int index)
 {
-	// ToDo:要素を所得して、indexがおかしかったら0を返そう
-	return -1;
+	if (index >= 0 && index < ar->num) {
+		
+		return ar->addr[index];
+	}
+	
+	return 0;
 }
 
-// my_array の要素数を取得する
+
 int size(const my_array* ar)
 {
-	// ToDo: 配列の要素数を返そう
-	return -1;
+	
+	return ar->num;
 }
