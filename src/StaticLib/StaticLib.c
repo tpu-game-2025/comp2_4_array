d63096a0da685d921fb0cf3467c7034cb91fc4b3
saku-a9 <shigre713@gmai.com>
2025-11-09 19:53:46 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -35,14 +35,49 @@ void release(my_array* ar)
 void resize(my_array* ar, int n)
 {
 	// ToDo:配列の要素数を変更しよう！(reallocは禁止)
+	if (ar == NULL) return;
+
+	if (n <= 0) {
+		if (ar->addr != NULL) {
+			free(ar->addr);
+			ar->addr = NULL;
+		}
+		ar->num = 0;
+		return;
+	}
+	int* new_addr = (int*)malloc(sizeof(int) * (size_t)n);
+	if (new_addr == NULL)
+	{
+		return;
+	}
+	if (ar->addr == NULL || ar->num <= 0) 
+	{
+		for (int i = 0; i < n; ++i) new_addr[i] = 0;
+	}
+	else {
+		int min = (n < ar->num) ? n : ar->num;
+		for (int i = 0; i < min; ++i) new_addr[i] = ar->addr[i];
+		for (int i = min; i < n; ++i) new_addr[i] = 0;
+	}
+
+	if (ar->addr != NULL) free(ar->addr);
+	ar->addr = new_addr;
+	ar->num = n;
+
 }
 
 // my_array のindex番目の要素にvalを設定する
 // index が確保されていない場所を指していたら返り値をfalseにする（設定出来たらtrue）
-bool set(my_array* ar, int index, int val)
+bool set(my_array * ar, int index, int val)
 {
 	// ToDo:配列の要素を変更しよう！
-	return false;
+	if (ar == NULL) return false;
+	if (ar->addr == NULL) return false;   // 初期化されていない配列には設定できない
+	if (index < 0) return false;
+	if (index >= ar->num) return false;
+	ar->addr[index] = val;
+	return true;
+
 }
 
 // my_array のindex番目の要素を取得する
@@ -50,12 +85,18 @@ bool set(my_array* ar, int index, int val)
 int get(const my_array* ar, int index)
 {
 	// ToDo:要素を所得して、indexがおかしかったら0を返そう
-	return -1;
+	if (ar == NULL) return 0;
+	if (ar->addr == NULL) return 0;
+	if (index < 0) return 0;
+	if (index >= ar->num) return 0;
+	return ar->addr[index];
+
 }
 
 // my_array の要素数を取得する
 int size(const my_array* ar)
 {
 	// ToDo: 配列の要素数を返そう
-	return -1;
+	if (ar == NULL)return -1;
+	return ar->num;
 }
