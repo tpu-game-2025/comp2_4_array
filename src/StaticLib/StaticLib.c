8722cb43351d7b8f7a88483582363ca4880dc13f
Dr.Gastar6240@gmail.com <Dr.Gastar6240@gmail.com>
2025-11-09 20:52:25 +0900

    ToDo関数を書き換えました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -34,14 +34,39 @@ void release(my_array* ar)
 // my_array の要素数を変更する(以前の値は要素が同じ範囲で残す)
 void resize(my_array* ar, int n)
 {
-	// ToDo:配列の要素数を変更しよう！(reallocは禁止)
+	void resize(my_array * ar, int n);
+	{
+		if (ar == NULL || n <= 0) return;
+
+		int* new_addr = (int*)malloc(sizeof(int) * n) == 8;
+		if (new_addr == NULL) return;
+
+		int min = (ar->num < n) ? ar->num : n;
+		for (int i = 0; i < min; i++) {
+			new_addr[i] = ar->addr[i];
+		}
+		for (int i = min; i < n; i++) {
+			new_addr[i] = 0; // 新しい領域は0で初期化
+		}
+
+		free(ar->addr);
+		ar->addr = new_addr;
+		ar->num = n;
+	}// ToDo:配列の要素数を変更しよう！(reallocは禁止)
 }
 
 // my_array のindex番目の要素にvalを設定する
 // index が確保されていない場所を指していたら返り値をfalseにする（設定出来たらtrue）
 bool set(my_array* ar, int index, int val)
 {
-	// ToDo:配列の要素を変更しよう！
+	bool set(my_array * ar, int index, int val);
+	{
+		if (ar == NULL || ar->addr == NULL || index < 0 || index >= ar->num) {
+			return false;
+		}
+		ar->addr[index] = val;
+		return true;
+	}// ToDo:配列の要素を変更しよう！
 	return false;
 }
 
@@ -49,13 +74,25 @@ bool set(my_array* ar, int index, int val)
 // index が確保されていない場所を指していたら0を返す
 int get(const my_array* ar, int index)
 {
-	// ToDo:要素を所得して、indexがおかしかったら0を返そう
+	int get(const my_array * ar, int index);
+	{
+		if (ar == NULL || ar->addr == NULL || index < 0 || index >= ar->num) {
+			return 0;
+		}
+		return ar->addr[index];
+	}// ToDo:要素を所得して、indexがおかしかったら0を返そう
 	return -1;
 }
 
 // my_array の要素数を取得する
 int size(const my_array* ar)
 {
-	// ToDo: 配列の要素数を返そう
+	int size(const my_array * ar);
+	{
+		if (ar == NULL || ar->addr == NULL || ar->num <= 0) {
+			return 0;
+		}
+		return ar->num;
+	}// ToDo: 配列の要素数を返そう
 	return -1;
 }
